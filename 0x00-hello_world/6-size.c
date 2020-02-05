33cb0b2753d1893c56a2da78ad90688601e48443
peluza <edisonisaza@gmail.com>
2020-02-05 16:45:27 -0500

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,10 @@
 */
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n");
+printf("Size of a char: 1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
+printf("Size of a long int: 8 byte(s)\n");
+printf("Size of a long long int: 8 byte(s)\n");
+printf("ize of a float: 4 byte(s)\n");
 return (0);
 }
