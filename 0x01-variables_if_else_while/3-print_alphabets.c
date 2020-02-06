641486cde5475c67897a9ca2d11455a60eb1a1d5
peluza <edisonisaza@gmail.com>
2020-02-06 16:57:38 -0500

    c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+char ED = 'a';
+
+for (ED = 'a'; ED <= 'Z'; ED++)
+{
+putchar(ED);
+}
+'\n';
+return (0);
+}
