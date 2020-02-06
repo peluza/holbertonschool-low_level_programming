c563d21bd1ce90196435b0b5b3582ec965493a26
peluza <edisonisaza@gmail.com>
2020-02-06 18:14:49 -0500

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+int ED;
+
+for (ED = '1'; ED <= '9'; ED++)
+{
+putchar(ED);
+}
+putchar('\n');
+return (0);
+}
