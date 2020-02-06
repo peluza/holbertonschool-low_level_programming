83a35ccdf2af990bef09487d323571196d4b0f07
peluza <edisonisaza@gmail.com>
2020-02-06 18:55:08 -0500

    c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ED;
+
+	for (ED = '0'; ED <= '9'; ED++)
+	{	
+	putchar(ED);
+
+		if (ED < 9)
+		{	
+		putchar(',');
+		putchar(' ');
+		}
+	}	
+	putchar('\n');
+	return (0);
+}
