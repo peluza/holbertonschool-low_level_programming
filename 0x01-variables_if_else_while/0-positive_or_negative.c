a71161faeee432453fba42cb3614a87f5976bd90
peluza <edisonisaza@gmail.com>
2020-02-06 13:45:42 -0500

    c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,15 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n>0)
+if (n > 0)
 {
-pirntf("%d is positive", n );
+pirntf("%d is positive", n);
 }
-else if(n<0)
+else if (n < 0)
 }
 printf("%d is negative", n);
 }
-else if(n==0)
+else if (n == 0)
 {
 printf("%d is zero", n);
 }
