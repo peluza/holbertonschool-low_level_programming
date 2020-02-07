c5056f0f1c38a24f6cf78c04b2bacfaccef4ad36
peluza <edisonisaza@gmail.com>
2020-02-06 19:37:52 -0500

    c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,21 +6,34 @@
  */
 int main(void)
 {
-        int E, D;
 
-        for (E = '0' ; E <= '9' ; E++)
-        {
-                for (D = '0' ; D <= '9' ; D++)
-                {
-                        putchar(E);
-                        putchar(D);
-                        if (!(E == '9' && D == '9'))
-                        {
-                        putchar(',');
-                        putchar(' ');
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+	int E, D;
+
+
+	for (E = '0' ; E <= '9' ; E++)
+
+	{
+
+		for (D = '0' ; D <= '9' ; D++)
+
+		{
+
+			putchar(E);
+
+			putchar(D);
+
+			if (!(E == '9' && D == '9'))
+
+			{
+
+				putchar(',');
+
+				putchar(' ');
+
+			}
+
+		}
 }
+putchar('\n');
+return (0);
+u}
