4fb5b480c78b0933dabe52085641575a172a67ac
peluza <edisonisaza@gmail.com>
2020-02-07 18:27:45 -0500

    c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,43 +7,25 @@
 int main(void)
 {
 
-	int a, b;
-
-
-	for (a = 0; a < 100; a++)
-
-	{
-
-		for (b = 0; b < 100; b++)
-
-		{
-
-			if (a < b)
-
-			{
-				putchar(a / 10 + '0');
-
-				putchar(a % 10 + '0');
-
-				putchar(' ');
-
-				putchar(b / 10 + '0');
-
-				putchar(b % 10 + '0');
-
-				if (a < 98)
-
-				{
-
-					putchar(',');
-
-					putchar(' ');
-
-				}
-
-			}
-
-		}
+int a, b;
+for (a = 0; a < 100; a++)
+{
+for (b = 0; b < 100; b++)
+{
+if (a < b)
+{
+putchar(a / 10 + '0');
+putchar(a % 10 + '0');
+putchar(' ');
+putchar(b / 10 + '0');
+putchar(b % 10 + '0');
+if (a < 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
 }
 putchar('\n');
 
