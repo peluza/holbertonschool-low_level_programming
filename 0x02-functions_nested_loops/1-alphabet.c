733538803ddcc0596c7e42c0262f8402ff8efaac
peluza <edisonisaza@gmail.com>
2020-02-10 09:39:50 -0500

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+int ED;
+
+for (ED = 'a'; ED <= 'z'; ED++)
+{
+putchar(ED);
+}
+putchar('\n');
+return (0);
+}
