172d8d796e7f26dc926e46bbd66d1af068c4b861
peluza <edisonisaza@gmail.com>
2020-02-10 18:30:20 -0500

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+int ED;
+
+for (ED = 'a'; ED <= 'z'; ED++)
+{
+while (ED = 'z')
+{
+	for ( ED = 'a'; ED <= 'z'; ED++) 
+		putchar(ED);
+	if ( ED = 'z')
+		putchar('\n');
+	if (ED = 'z'; )
+		break;
+}
+}
+return (0);
+}
