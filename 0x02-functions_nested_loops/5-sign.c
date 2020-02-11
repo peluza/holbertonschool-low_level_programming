cda425a30a1d47ed97caebdad5903a3718a12026
peluza <edisonisaza@gmail.com>
2020-02-11 18:54:44 -0500

    c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,17 +1,19 @@
 #include "holberton.h"
+
 /**
-* _islower - exclude letter c
-*
+* print_sign: print singn the number
+*@n: variable numeric
 * Return: Always 0 (Success)
 */
-int print_sign(int n)
-{
 
-        if (n == 0)
-        {
+int print_sign(int n)
+
+{
+	if (n == 0)
+	{
 		_putchar('0');
 		return (0);
-        }
+	}
 	if (n < 0)
 	{
 		_putchar('-');
@@ -20,7 +22,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-                return (1);
+		return (1);
 	}
 	return (0);
 }
