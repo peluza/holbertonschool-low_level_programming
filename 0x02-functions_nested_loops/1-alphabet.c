652c40a659f48ecbee05b20621b3704a3f73b486
peluza <edisonisaza@gmail.com>
2020-02-11 12:47:18 -0500

    c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,15 @@
-#include <stdio.h>
 #include "holberton.h"
 /**
-* main - Entry point
+* print_alphabet - print alphabet
 *
 * Return: Always 0 (Success)
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-
 int ED;
-
 for (ED = 'a'; ED <= 'z'; ED++)
 {
 _putchar(ED);
 }
 _putchar('\n');
-return (0);
 }
