05d13c443fdc146d15e12eacb4fb20f2b2e1fa6f
peluza <edisonisaza@gmail.com>
2020-02-11 18:29:35 -0500

    c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,14 @@
 #include "holberton.h"
+
 /**
 * print_alphabet_x10 - print alphabetx10
-*
+*@ED - variable
+*@i - variable
 * Return: Always 0 (Success)
 */
+
 void print_alphabet_x10(void)
+
 {
 
 	int ED;
