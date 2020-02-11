84883319f5d9f006d000a6a3173b8264cdccb599
peluza <edisonisaza@gmail.com>
2020-02-11 16:59:02 -0500

    c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* print_last_digit - print_last_digit
+*
+* Return: Always 0 (Success)
+*/
+
+int print_last_digit(int n)
+{
+	if (n > 0)
+	{
+		n = n % 10;
+	    _putchar(n + '0');
+	    return (n);
+
+	}
+
+	else
+	{
+		n = n * -1;
+		n = n % 10;
+		_putchar(n + '0');
+		return (n);
+	}
+
+}
