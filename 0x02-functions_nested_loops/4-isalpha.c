101118027d9a83d5f8c0772e250140a044cd840a
peluza <edisonisaza@gmail.com>
2020-02-11 18:46:42 -0500

    c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-* _isalpha - 
+* _isalpha - description
 *@c: varibale
 * Return: Always 0 (Success)
 */
@@ -9,14 +9,13 @@
 int _isalpha(int c)
 
 {
-
-        if (c >= 97 && c <= 122)
-        {
-                return (1);
-        }
-        if (c >= 65  && c <= 90)        
+	if (c >= 97 && c <= 122)
+	{
 		return (1);
-	
+	}
+	if (c >= 65  && c <= 90)
+	{
+		return (1);
+	}
 	return (0);
-
 }
