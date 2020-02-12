c304369a520f88cf88468aa6fc4cf05c68e973b9
peluza <edisonisaza@gmail.com>
2020-02-11 19:50:09 -0500

    c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,19 +11,21 @@
 int print_last_digit(int n)
 
 {
+
 	if (n > 0)
 	{
 		n = n % 10;
 		_putchar(n + '0');
 		return (n);
 	}
-
 	else
 	{
 		n = n * -1;
 		n = n % 10;
 		_putchar(n + '0');
 		return (n);
-	}
 
+	}
 }
+
+
