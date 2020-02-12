d91d7d5d880f35873bfcb0f89ce5a1e30cdbaaeb
peluza <edisonisaza@gmail.com>
2020-02-12 13:53:48 -0500

    c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
         i = 0;
 
-        while (i < 10) /*the number is alwer 0*/
+        /*while (i < 10) the number is alwer 0
         {
                 putchar(i);
-        }
+        }*/
 
         printf("Infinite loop avoided! \\o/\n");
 
