e3b6d989c76377a35d6290cef544abd0d40784e3
peluza <edisonisaza@gmail.com>
2020-02-12 11:27:00 -0500

    c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a > c)
 
 	{
 
@@ -20,7 +20,7 @@ int largest_number(int a, int b, int c)
 
 	}
 
-	else if (b > a && b > c)
+	else if (b > a && b => c)
 
 	{
 
