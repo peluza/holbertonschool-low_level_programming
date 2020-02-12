77a57a75e4cf078428a51e4fbad4952d58335739
peluza <edisonisaza@gmail.com>
2020-02-12 13:10:45 -0500

    c

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,7 +1,9 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
 
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 
-#endif
+#endif /* HOLBERTON_H */
