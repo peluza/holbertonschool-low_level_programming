f7c670a3d1242c706f3d5d300c53649eb4193104
peluza <edisonisaza@gmail.com>
2020-02-11 23:16:53 -0500

    c

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int largest_number(int a, int b, int c);
+int add(int, int);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+void print_alphabet_x10(void);
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif
