da0d5bbc98efb6680c9d0b48943140a76d1f9be8
peluza <edisonisaza@gmail.com>
2020-02-13 22:38:15 -0500

    c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,17 +15,17 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
 		{
 			printf("%s", "FizzBuzz");
 		}
 
-		else if (i % 3 == 0)
+		else if ((i % 3) == 0)
 		{
 			printf("%s", "Fizz");
 		}
 
-		else if (i % 5 == 0)
+		else if ((i % 5) == 0)
 		{
 			printf("%s", "Buzz");
 		}
