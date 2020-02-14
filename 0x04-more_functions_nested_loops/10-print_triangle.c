f496ed6193474debaa07ac8ef54f0b2ffc7f8420
peluza <edisonisaza@gmail.com>
2020-02-13 19:40:47 -0500

    c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+* print_square - the print a square
+*
+* @size: numbers the line
+*
+* Return: Always 0 (Success)
+*/
+
+void print_triangle(int size)
+
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+
+	{
+		for (i = 1; i <= size; i++)
+		{
+		for (j = 1; j <= size - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
+		_putchar('#');
+		}
+		_putchar('\n');
+		}
+	}
+
+}
