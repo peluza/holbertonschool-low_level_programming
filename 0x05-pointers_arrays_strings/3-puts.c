f06befcc087c4b2210822955e4dbe78f96c04232
peluza <edisonisaza@gmail.com>
2020-02-18 13:53:37 -0500

    c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,15 +11,12 @@
 
 void _puts(char *str)
 {
-	char a;
 	int x;
 
-	a = str[0];
 
-	for (x = 0; a != '\0'; x++)
+	for (x = 0; str[x] != '\0'; x++)
 	{
-		a = str[x];
-		_putchar(a);
+		_putchar(str[x]);
 	}
 _putchar('\n');
 
