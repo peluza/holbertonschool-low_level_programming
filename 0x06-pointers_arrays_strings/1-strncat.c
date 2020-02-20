3501652a05bd9ed2cd2630f04c520333570ed05f
peluza <edisonisaza@gmail.com>
2020-02-20 15:28:12 -0500

    c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
  * _strncat - the funtion is cocatene in the n
- * @n: the number
- * @dest: punter one
- * @src: punter two
+ * @s: the command _strlen
  * Return: dest
  */
+
+int _strlen(char *s);
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int x = 0;
-	int dest_len = strlen(dest);
+	int dest_len = _strlen(dest);
 
 	for (x = 0; x < n && src[x] != '\0'; x++)
 	{
@@ -22,4 +20,31 @@ char *_strncat(char *dest, char *src, int n)
 	dest[dest_len + x] = '\0';
 
 return (dest);
+}
+
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen - long of the str
+ *
+ * @s: the caracter
+ *
+ * Return: Always c.
+ */
+
+int _strlen(char *s)
+{
+	char a;
+	int x, c;
+
+	a = s[0];
+
+	for (x = 0; a != '\0'; x++)
+	{
+		a = s[x];
+		c = x;
+	}
+
+return (c);
 }
