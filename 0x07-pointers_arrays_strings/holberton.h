12c4e774e9ad095a58fbbda964832615904ef130
peluza <edisonisaza@gmail.com>
2020-02-24 16:05:58 -0500

    c

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
