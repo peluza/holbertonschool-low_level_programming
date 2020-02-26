2eacbeaeecdf54fb52fc56978b3bcb5d40ec82d6
peluza <edisonisaza@gmail.com>
2020-02-26 15:35:05 -0500

    c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - print in reverse used the recursion
+ *
+ * @s: the punters a evalue
+ *
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s <= '\0')
+	{
+		_putchar(*s);
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
 int _putchar(char c);
 
