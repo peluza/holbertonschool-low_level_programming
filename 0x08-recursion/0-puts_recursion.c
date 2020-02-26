1941aed5a26f2fa7e4fa5d4c7c29944dde6db2a4
peluza <edisonisaza@gmail.com>
2020-02-26 14:10:11 -0500

    c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - print used recursion
+ *
+ * @s: the punters a evalue
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
