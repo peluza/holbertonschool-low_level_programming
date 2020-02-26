bf78ea58fdc4286b858a62242bc163c74d9a96ac
peluza <edisonisaza@gmail.com>
2020-02-26 17:23:58 -0500

    c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number  - the number prime number 
+ * 
+ * @n: the character number
+ *
+ * Return: the result.
+ */
+
+int is_prime_number(int n)
+{
+	if (n > 0)
+	{
+
+	}
+}
+
