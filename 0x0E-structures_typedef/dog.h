11a8a756ea8eb628dbe270cb2ef0fa6f7e6f357b
peluza <edisonisaza@gmail.com>
2020-03-09 15:05:16 -0500

    c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - the strug dog
+ * @name: name the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
