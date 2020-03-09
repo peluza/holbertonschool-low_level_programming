87a56395037ec7f0c5ea339dae42571e915604e9
peluza <edisonisaza@gmail.com>
2020-03-09 17:18:40 -0500

    c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,15 +4,16 @@
 /**
  * print_dog - print the struct dog
  * @d: punters
+ * Return void
  */
 
 void print_dog(struct dog *d)
 {
 	if (d != 0)
 	{
-		if (d->name == 0)
+		if ((d->name) == 0)
 			(*d).name = "(nil)";
-		if (d->owner == 0)
+		if ((d->owner) == 0)
 			(*d).owner = "(nil)";
 		printf("name: %s\n", (*d).name);
 		printf("age: %f\n", (*d).age);
