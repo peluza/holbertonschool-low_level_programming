0c12a89bd4d722b8142c7e70c4cf982e72a524f8
peluza <edisonisaza@gmail.com>
2020-03-09 18:25:51 -0500

    c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - create new copy the dog
+ * @d: punters
+ * @name: name the new dog
+ * @age: the age of the new dog
+ * @owner: owner of the new dog
+ * Return void
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if ((d.name) == 0)
+	{
+		return (0);
+	}
+
+	if ((d.age) == 0)
+	{
+		return (0);
+	}
+
+	if ((d.owner) == 0)
+	{
+		return (0);
+	}
+
+}
+
