2725b206d63c48415fa906eeb75777b44de014af
peluza <edisonisaza@gmail.com>
2020-03-09 12:23:14 -0500

    c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _realloc - prints the remplace the chars
+ * @old_size: old the size
+ * @new_size: new the siza
+ *
+ * Return: the punters
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+		int *ptr;
+		int e;
+
+
+
+
+
+
+
+
+
+
+
+
+		ptr = malloc(old_size);
+		old_size = ptr;
+		new_size = e;
+		ptr = new_size;
+
+		if (new_size > old_size)
+		{
+			break;
+
+		}
+
+		if (new_size == old_size)
+		{
+			return (ptr);
+
+		}
+
+		if (ptr == NULL)
+		{
+			ptr = malloc(old_size);
+		}
+
+		if (new_size == 0 && prt != NULL)
+		{
+			free(ptr);
+			return (NULL);
+		}
+
+}
