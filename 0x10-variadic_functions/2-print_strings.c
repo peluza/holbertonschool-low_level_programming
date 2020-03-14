58349f6b13810800a55628093d2f6a04c9c7c964
peluza <edisonisaza@gmail.com>
2020-03-14 13:40:22 -0500

    c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,9 +27,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			else
 			{
 				printf("%s", a);
-				if (i < (n - 1))
-					printf("%s", separator);
+
 			}
+			if (i < (n - 1))
+				printf("%s", separator);
+
 		}
 		else
 		{
