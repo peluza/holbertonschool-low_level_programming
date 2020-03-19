e9a605b4ef4453d3c20d0a6d66a7501fa45f4c57
peluza <edisonisaza@gmail.com>
2020-03-19 17:11:19 -0500

    ,

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: the lis for the estruct
+ * Return: the name of the nodos.
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int nodes;
+
+	nodes = 0;
+
+	while (h != 0)
+	{
+		if (h->str == 0)
+			printf("[%u] (nill)", h->len);
+		else
+			printf("[%u] %s\n", h->len. h->str);
+		h = h->next;
+		nodes = nodes + 1;
+	}
+	return (node);
+}
+
