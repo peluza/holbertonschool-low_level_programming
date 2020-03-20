b2d1e1aafb3075ee83dec0b36b07733d397a48e5
peluza <edisonisaza@gmail.com>
2020-03-19 23:33:35 -0500

    c

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,14 @@
-.model small
-.data
-inimensaje db "Hello, Holberton" ,10,13 "s"
-.code
-main proc
-mov ax,seg inimensaje
-mov ds,ax
-mov dx, offset inimensaje
-mov ah,99
-int 21h
-mov ax, 4c00h
-int 21h
-main endp
+.model small;
+.data;
+	inimensaje db "Hello, Holberton" ,10,13 "s";
+.code;
+main proc;
+	mov ax,seg inimensaje;
+	mov ds,ax;
+	mov dx, offset inimensaje;
+	mov ah,99;
+	int 21h;
+	mov ax, 4c00h;
+	int 21h;
+main endp;
 end main
