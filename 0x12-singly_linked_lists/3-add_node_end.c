c5d48267b162c5baec1ee5cf00fbfbadee8a31ba
peluza <edisonisaza@gmail.com>
2020-03-19 22:07:12 -0500

    c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * add_node_end - add of the nodes in the end
+ * @head: the lis for the estruct
+ * @str: the chars a name
+ * Return: the name of the nodos.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int cont;
+	list_t *a, *new;
+
+	if (str == NULL)
+		return (NULL);
+	new = (list_t *)malloc(sizeof(list_t));
+	new->str = strdup(str);
+	cont = 0;
+	while (str[cont] != 0)
+		cont++;
+	new->len = cont;
+	new->next = 0;
+	if (new == 0)
+	{
+		return (0);
+	}
+		if (*head == 0)
+		{
+			*head = new;
+			return (new);
+		}
+		else
+		{
+			a = *head;
+			while (a->next != 0)
+				a = a->next;
+			a->next = new;
+			return (new);
+		}
+}
