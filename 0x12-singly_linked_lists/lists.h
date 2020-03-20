371fd4d1d15509d9d7f9277d9bc89184d3402aff
peluza <edisonisaza@gmail.com>
2020-03-20 01:37:01 -0500

    c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,9 +17,9 @@
 
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -27,5 +27,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void before (void) __attribute__ ((constructor));
+void before(void) __attribute__ ((constructor));
 #endif
