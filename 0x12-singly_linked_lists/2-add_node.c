f41e21d7b7eae3f0e1a8e2f1d653267f2f50eaa5
peluza <edisonisaza@gmail.com>
2020-03-19 19:55:27 -0500

    c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * add_node - add of the nodes
+ * @head: the lis for the estruct
+ * @str: the chars a name
+ * Return: the name of the nodos.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int cont;
+	list_t *new;
+
+	new = (list_t *)malloc(sizeof(list_t));
+	new->str = strdup(str);
+	cont = 0;
+	while (str[cont] != 0)
+		cont++;
+	new->len = cont;
+	new->next = *head;
+	*head = new;
+	if (new == 0)
+	{
+		return (0);
+	}
+	return (new);
+	free(new->str);
+	free(new);
+}
