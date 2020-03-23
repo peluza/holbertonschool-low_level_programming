6ec7191c2e31911dbd33a2dd44bee636b360841b
peluza <edisonisaza@gmail.com>
2020-03-23 15:39:53 -0500

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free the memory
+ * @head: the lis for the estruct
+ */
+
+void free_listint2(listint_t **head)
+{
+
+	if (*head == NULL)
+	{
+		return;
+	}
+	while (*head != NULL)
+	{
+
+		free(*head);
+		*head = (*head)->next;
+	}
+	head = NULL;
+}
