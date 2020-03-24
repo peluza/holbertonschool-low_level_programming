efac0ae520a16e8c0b325348904bfaa74dbe79a0
peluza <edisonisaza@gmail.com>
2020-03-24 16:14:04 -0500

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *new, *a = *head;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 	if (idx == 0)
 	{
