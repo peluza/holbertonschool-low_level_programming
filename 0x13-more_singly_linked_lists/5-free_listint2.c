accebbd1554e5f141f7657b974b61b1d5265a02a
peluza <edisonisaza@gmail.com>
2020-03-24 10:53:48 -0500

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,15 @@
 
 void free_listint2(listint_t **head)
 {
+	listint_t *a;
 
 	if (*head == NULL)
 		return;
 	while (*head != NULL)
 	{
+		a = (*head)->next;
 		free(*head);
-		*head = (*head)->next;
+		*head = a;
 	}
 	head = NULL;
 }
