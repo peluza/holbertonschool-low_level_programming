0f4f4b3ec43c5996b9767b83adc65a44df6a3731
peluza <edisonisaza@gmail.com>
2020-03-31 18:48:05 -0500

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int fp, w, i;
 	mode_t mode;
 
-	mode = S_IRUSR | S_IWUSR | O_TRUNC;
+	mode = O_RDWR | O_TRUNC | S_IRUSR | S_IWUSR;
 	if (filename == NULL)
 		return (0);
 	fp = creat(filename, mode);
