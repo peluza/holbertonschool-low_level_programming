c0516e4b75d9b89b80e67599555be00d200d84f6
peluza <edisonisaza@gmail.com>
2020-03-31 18:41:12 -0500

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * create_file - create to file
+ * @filename: file the name at create
+ * @text_content: content of the text
+ *
+ * Return: Always 0.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp, w, i;
+	mode_t mode;
+
+	mode = S_IRUSR | S_IWUSR | O_TRUNC;
+	if (filename == NULL)
+		return (0);
+	fp = creat(filename, mode);
+	if (fp == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		i = strlen(text_content);
+		w = write(fp, text_content, i);
+		if (w == -1)
+		{
+			write(STDOUT_FILENO, "fails", 6);
+			return (-1);
+		}
+	}
+	close(fp);
+	return (1);
+}
