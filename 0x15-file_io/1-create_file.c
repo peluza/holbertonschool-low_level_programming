5fcd1904a7ce4364da1b64f7493f100e98673cc1
peluza <edisonisaza@gmail.com>
2020-03-31 19:24:38 -0500

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,23 +13,19 @@ int create_file(const char *filename, char *text_content)
 	int fp, w, i;
 	mode_t mode;
 
-	mode = O_RDWR | O_TRUNC | S_IRUSR | S_IWUSR;
+	mode = O_RDWR | O_CREAT | O_TRUNC;
 	if (filename == NULL)
 		return (-1);
-	fp = creat(filename, mode);
+	if (text_content == NULL)
+		text_content = "";
+	i = strlen(text_content);
+	fp = open(filename, mode, 0600);
 	if (fp == -1)
-		return (-1);
-	if (text_content != NULL)
+		return (fp);
+	w = write(fp, text_content, i);
+	if (w == -1)
 	{
-		for (i = 0; text_content[i] != 0; i++)
-		{
-		}
-		w = write(fp, text_content, i);
-		if (w == -1)
-		{
-			write(STDOUT_FILENO, "fails", 6);
-			return (-1);
-		}
+		return (-1);
 	}
 	close(fp);
 	return (1);
