a09a0b9bb9057d0cd01324e25a4e9286fa687952
peluza <edisonisaza@gmail.com>
2020-05-03 18:38:11 -0500

    8-delete_dnodeint.c

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,72 @@
+#include "lists.h"
+
+/**
+ *delete_dnodeint_at_index - delete at vlaue the dlistint_t
+ *@head: the chars of the lists
+ *@index: position of the nodes
+ *Return: the value at delete
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = *head;
+	unsigned int i = 0, len = 0;
+
+	if (*head == 0)
+		return (-1);
+	if ((*head)->next == 0 && index == 0)
+	{
+		free(*head);
+		return (-1);
+	}
+	len = dlistint_len(*head);
+	len -= 1;
+	if (index > len)
+		return (-1);
+	if (index == 0)
+	{
+		tmp->next->prev = 0;
+		*head = tmp->next;
+		free(tmp);
+		tmp = 0;
+		return (1);
+	}
+	for (i = 0; i != index; i++)
+		tmp = tmp->next;
+	if (index == len)
+	{
+		tmp->prev->next = 0;
+		free(tmp);
+		tmp = 0;
+		return (1);
+	}
+	else
+	{
+		tmp->prev->next = tmp->next;
+		tmp->next->prev = tmp->prev;
+		tmp->prev = 0;
+		tmp->next = 0;
+		free(tmp);
+		tmp = 0;
+		return (1);
+	}
+	return (1);
+}
+/**
+ *dlistint_len - prints all numbers the elemts of the dlistsint_s
+ *@h: the lists for the estruct
+ *Return:  the name of the nodos
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int nodes = 0;
+
+	if (h == 0)
+		return (0);
+	while (h != 0)
+	{
+		h = h->next;
+		nodes += 1;
+	}
+	return (nodes);
+}
