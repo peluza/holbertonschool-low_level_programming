aee5dc33da207078769305c60b3a5c191c8bf4de
peluza <edisonisaza@gmail.com>
2020-05-03 12:26:59 -0500

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *dlistint_len - prints all numbers the elemts of the dlistsint_s
+ *@h: the lists for the estruct
+ *Return:  the name of the nodos
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+        unsigned int nodes = 0;
+
+        if (h == 0)
+                return (0);
+        while (h != 0)
+        {
+                h = h->next;
+                nodes += 1;
+        }
+        return (nodes);
+}
