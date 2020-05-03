1efdb9f57e98bb550efb95ba67e94a7b6454e234
peluza <edisonisaza@gmail.com>
2020-05-03 12:55:08 -0500

    2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ *add_dnodeint - add the nodes of the dlistint_t
+ *@head: the chars of the lists
+ *n: the number coitainrs in the lists
+ *Return: of the head
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (head == 0)
+		return (0);
+	new =  malloc(sizeof(dlistint_t));
+	if (new == 0)
+		return (0);
+	new->n = n;
+	new->next = 0;
+	new->prev = 0;
+	if (*head == 0)
+		*head = new;
+	else
+	{
+		(*head)->prev = new;
+		new->next = *head;
+		*head = new;
+	}
+	return (*head);
+}
