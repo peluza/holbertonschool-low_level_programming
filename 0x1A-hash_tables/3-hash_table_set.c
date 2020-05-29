662b92e6fb1e83b4e0ea34404b82555becdb1ede
peluza <edisonisaza@gmail.com>
2020-05-29 14:10:41 -0500

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,7 +9,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *tmp1, *new_node;
+	hash_node_t *tmp1 = NULL, *new_node = NULL;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
@@ -22,6 +22,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			if (tmp1->value)
 				free(tmp1->value);
 			tmp1->value = strdup(value);
+			if (tmp1->value == NULL)
+				return (0);
 			return (1);
 		}
 		tmp1 = tmp1->next;
@@ -30,7 +32,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
