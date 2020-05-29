d25bc1b572f3b8fc58ba57a780f939581782f087
peluza <edisonisaza@gmail.com>
2020-05-29 00:08:47 -0500

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,10 +1,15 @@
 #include "hash_tables.h"
-
+/**
+ * key_index - the result of the hash
+ * @key: the char is the key
+ * @size:is the size of the list
+ * Return: hash value
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-        int result, res_key;
+	int result, res_key;
 
-        res_key = hash_djb2(key);
-        result = res_key % size;
-        return (result);
+	res_key = hash_djb2(key);
+	result = res_key % size;
+	return (result);
 }
