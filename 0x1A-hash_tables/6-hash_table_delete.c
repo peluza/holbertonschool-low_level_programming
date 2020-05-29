622f46577f86cb77753e3d19bd27173796485f9c
peluza <edisonisaza@gmail.com>
2020-05-29 14:22:08 -0500

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,19 +1,24 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_delete - the memeory free
+ * @ht: is the size of the list
+ * Return: hash value
+ */
 void hash_table_delete(hash_table_t *ht)
 {
-        unsigned long int i, size;
+	unsigned long int i, size;
 
-        if (ht == NULL || ht->array == NULL || ht->size == 0)
-                return;
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
 
-        size = ht->size;
-        for (i = 0; i < size; i++)
-                free_list(ht->array[i]);
-        free(ht->array);
-        ht->array = NULL;
-        ht->size = 0;
-        free(ht);
+	size = ht->size;
+	for (i = 0; i < size; i++)
+	{
+		if (ht->array[i] != NULL)
+			free_list(ht->array[i]);
+	}
+	free(ht->array);
+	free(ht);
 }
 /**
  * free_list - free the memory
@@ -21,11 +26,22 @@ void hash_table_delete(hash_table_t *ht)
  */
 void free_list(hash_node_t *head)
 {
-        if (head == NULL)
-                return;
-        if ((*head).next != NULL)
-                free_list(head->next);
-        free(head->key);
-        free(head->value);
-        free(head);
+	hash_node_t *current;
+	hash_node_t *n;
+
+	current = head;
+
+	if (head)
+	{
+		while (current != NULL)
+		{
+			if (current->key)
+				free(current->key);
+			if (current->value)
+				free(current->value);
+			n = current->next;
+			free(current);
+			current = n;
+		}
+	}
 }
