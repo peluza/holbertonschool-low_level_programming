3825ce34e09cef95a01a13320ae54f3ca36c3aae
peluza <edisonisaza@gmail.com>
2020-05-29 11:20:35 -0500

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -3,15 +3,21 @@
 void hash_table_delete(hash_table_t *ht)
 {
         unsigned long int i, size;
+        hash_node_t *tmp = NULL, *aux = NULL;
 
         size = ht->size;
         for (i = 0; i < size; i++)
         {
-                if (ht->array[i])
+                aux = ht->array[i];
+                while (aux)
                 {
-                        free(ht->array[i]->key);
-                        free(ht->array[i]->value);
+                        free(aux->key);
+                        free(aux->value);
+                        tmp = aux->next;
+                        free(aux);
+                        aux = tmp;
                 }
+                free(aux);
         }
         free(ht->array);
         free(ht);
