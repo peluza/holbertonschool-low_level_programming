eef7f78b6256d9f82afed89460eca89bab2bea8d
peluza <edisonisaza@gmail.com>
2020-05-29 00:47:48 -0500

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,20 +1,27 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_get - the hash table the get
+ * @ht: is the size of the list
+ * @key: the char is the key
+ * Return: hash value
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-        unsigned long int index;
+	unsigned long int index;
+	hash_node_t *inter;
 
-        if (ht == NULL || key == NULL)
-                return (NULL);
+	if (ht == NULL || key == NULL)
+		return (NULL);
 
-        index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
-        if (ht->array[index])
-        {
-                if (strcmp(ht->array[index]->key, key) == 0)
-                {
-                        return (ht->array[index]->value);
-                }
-        }
-        return (NULL);
+	while (ht->array[index])
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+		{
+			return (ht->array[index]->value);
+		}
+		inter = inter->next;
+	}
+	return (NULL);
 }
