34575f9c6f64b2b3a4bffb89fa3c478b38bf573d
peluza <edisonisaza@gmail.com>
2020-05-29 00:15:49 -0500

    hash_tables.h

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,24 +10,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 	hash_node_t **new_array;
-	unsigned long int i;
 
-	if (size < 0)
+	if (size < 1)
 		return (0);
-
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
-	new_array = malloc(sizeof(hash_node_t *) * size);
+	new_array = calloc(size, sizeof(hash_node_t *));
 	if (new_array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
-	{
-		new_array[i] = NULL;
-	}
+	new_table->size = size;
 	new_table->array = new_array;
 	return (new_table);
 }
