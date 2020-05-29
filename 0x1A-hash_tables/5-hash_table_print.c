75d9a995a04a87da9e26bba77440305da2a55c2f
peluza <edisonisaza@gmail.com>
2020-05-29 00:49:57 -0500

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,22 +1,26 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_print - the print is the table
+ * @ht: is the size of the list
+ * Return: hash value
+ */
 void hash_table_print(const hash_table_t *ht)
 {
-        unsigned int i = 0;
-        int inter = 0;
+	unsigned int i = 0;
+	int inter = 0;
 
-        if (ht == NULL)
-                return;
-        printf("{");
-        for (i = 0; i < ht->size; i++)
-        {
-                if (ht->array[i])
-                {
-                        if (inter > 0)
-                                printf(", ");
-                        printf("\'%s\': \'%s\'", ht->array[i]->key, ht->array[i]->value);
-                        inter++;
-                }
-        }
-        printf("}\n");
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			if (inter > 0)
+				printf(", ");
+			printf("\'%s\': \'%s\'", ht->array[i]->key, ht->array[i]->value);
+			inter++;
+		}
+	}
+	printf("}\n");
 }
