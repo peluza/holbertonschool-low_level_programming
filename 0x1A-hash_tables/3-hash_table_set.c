e9b0a9da1da605e221c25742407a349a2767c36d
peluza <edisonisaza@gmail.com>
2020-05-29 00:35:27 -0500

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,6 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				tmp->value = strdup(value);
 			if (tmp->value == NULL)
 				return (0);
+			return (1);
 		}
 		tmp = tmp->next;
 	}
