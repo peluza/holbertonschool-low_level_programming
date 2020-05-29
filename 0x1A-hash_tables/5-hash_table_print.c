ee2cd20f09a046641cbaef5de5ebd43ab9ae0f0e
peluza <edisonisaza@gmail.com>
2020-05-29 00:55:11 -0500

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -18,7 +18,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (inter > 0)
 				printf(", ");
-			printf("\'%s\': \'%s\'", ht->array[i]->key, ht->array[i]->value);
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 			inter++;
 		}
 	}
