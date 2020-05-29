805fa26a33cadfa347fa3e36c86326111d9a6297
peluza <edisonisaza@gmail.com>
2020-05-29 09:50:31 -0500

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,31 +8,52 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *tmp1 = NULL, *new_node = NULL;
+	hash_node_t *r;
+	hash_node_t *node_head;
+	unsigned long int _idx;
+	char *empty_str = "\0";
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL || key == NULL || strcmp(key, empty_str) == 0)
 		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	tmp1 = ht->array[index];
-	while (tmp1)
+	_idx = key_index((unsigned char *)key, ht->size);
+	node_head = ht->array[_idx];
+	r = add_node(&node_head, key, value);
+	ht->array[_idx] = node_head;
+	if (r == NULL)
+		return (0);
+	return (1);
+}
+
+/**
+ *add_node - adds a new node 
+ *@head: is the size of the list
+ *@key: the char is the value
+ *@value: the char is the key
+ *Return: a pointer to the new head
+ */
+
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	hash_node_t *temp = NULL;
+
+	temp = *head;
+	while (temp)
 	{
-		if (strcmp(tmp1->key, key) == 0)
+		if (strcmp(key, temp->key) == 0)
 		{
-			free(tmp1->value);
-			tmp1->value = strdup(value);
-			if (tmp1->value == NULL)
-				return (0);
-			return (1);
+			free(temp->value);
+			temp->value = strdup(value);
+			return (*head);
 		}
-		tmp1 = tmp1->next;
+		temp = temp->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return (0);
+		return (NULL);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-	return (1);
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
 }
