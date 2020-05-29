507693a13a3b0c5e6ffb61302a0be382ab4b94f6
peluza <edisonisaza@gmail.com>
2020-05-29 13:35:05 -0500

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -3,22 +3,29 @@
 void hash_table_delete(hash_table_t *ht)
 {
         unsigned long int i, size;
-        hash_node_t *tmp = NULL, *aux = NULL;
+
+        if (ht == NULL || ht->array == NULL || ht->size == 0)
+                return;
 
         size = ht->size;
         for (i = 0; i < size; i++)
-        {
-                aux = ht->array[i];
-                while (aux)
-                {
-                        free(aux->key);
-                        free(aux->value);
-                        tmp = aux->next;
-                        free(aux);
-                        aux = tmp;
-                }
-                free(aux);
-        }
+                free_list(ht->array[i]);
         free(ht->array);
+        ht->array = NULL;
+        ht->size = 0;
         free(ht);
+}
+/**
+ * free_list - free the memory
+ * @head: the lis for the estruct
+ */
+void free_list(hash_node_t *head)
+{
+        if (head == NULL)
+                return;
+        if ((*head).next != NULL)
+                free_list(head->next);
+        free(head->key);
+        free(head->value);
+        free(head);
 }
