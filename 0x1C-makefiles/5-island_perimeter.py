4cb9bd4b604dfb7cee26d14c63cd964464dce6dd
peluza <edisonisaza@gmail.com>
2020-07-04 00:44:06 +0000

    task 5 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" 5- island_perimeter
+    """
+
+
+def island_perimeter(grid):
+    """island_perimeter
+
+    Args:
+        grid (list): the date in the list is intergert
+    Returns:
+        [int]: perimeter
+    """
+
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if grid[i-1][j] == 0:
+                    count += 1
+                if grid[i+1][j] == 0 or i == len(grid) - 1:
+                    count += 1
+                if grid[i][j+1] == 0 or j == len(grid[i]) - 1:
+                    count += 1
+                if grid[i][j-1] == 0:
+                    count += 1
+    return count
