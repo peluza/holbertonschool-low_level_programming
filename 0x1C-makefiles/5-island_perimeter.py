3ae0f4af850a320df20ea8c6e77db2241e351f21
peluza <edisonisaza@gmail.com>
2020-07-04 00:46:00 +0000

    task 5 5-island_perimeter.py add line 19

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,7 +16,7 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i-1][j] == 0:
+                if grid[i-1][j] == 0 or i == 0:
                     count += 1
                 if grid[i+1][j] == 0 or i == len(grid) - 1:
                     count += 1
