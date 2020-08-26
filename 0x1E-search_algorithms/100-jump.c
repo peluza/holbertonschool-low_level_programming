2efd480b751b023c95bc79cf66f5e1981e194f3e
peluza <edisonisaza@gmail.com>
2020-08-26 00:07:28 +0000

    100-jump.c

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,53 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - Jump search algorithm
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+        size_t a = 0, b = sqrt(size);
+
+        if (array == NULL)
+                return (-1);
+
+        while (array[min(b, size) - 1] < value)
+        {
+                printf("v_2Value checked array[%ld] = [%d]\n", a, array[a]);
+                a = b;
+                b = b + sqrt(size);
+                if (a >= size)
+                        return (-1);
+        }
+
+        while (array[a] < value)
+        {
+                printf("v_3Value checked array[%ld] = [%d]\n", a, array[a]);
+                a++;
+                if (a == min(b, size))
+                        return (-1);
+        }
+
+        if (array[a] == value)
+        {
+                printf("v_4Value checked array[%ld] = [%d]\n", a, array[a]);
+                return (a);
+        }
+        return (-1);
+}
+
+/**
+ * min - number min 
+ *
+ * Return: number min
+ */
+
+size_t min(size_t num1, size_t num2)
+{
+        if (num1 > num2)
+                return (num2);
+        else
+                return (num1);
+}
